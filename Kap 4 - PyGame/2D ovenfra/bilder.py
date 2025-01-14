48adf07ba6664384aed7b3fa7addc85646aca01f
Rasmus Graver <rasmus.graver@gmail.com>
2025-01-14 12:57:23 +0100

    Pygame 2D ovenfra

---
+++ Kap 4 - PyGame/2D ovenfra/bilder.py
@@ -0,0 +1,7 @@
+import pygame as pg
+
+bg_image = pg.image.load("assets/bg.png").convert_alpha()
+link_d = pg.image.load("assets/link_d.png").convert_alpha()
+link_u = pg.image.load("assets/link_u.png").convert_alpha()
+link_r = pg.image.load("assets/link_r.png").convert_alpha()
+link_l = pg.transform.flip(link_r, True, False)
