5c66ebe560990a26c2b64aaf2088b351d19dd7c9
Rasmus Graver <rasmus.graver@gmail.com>
2025-01-21 14:35:56 +0100

    Update aksje oppg1b.py

--- Tentamen H24/aksje oppg1b.py
+++ Tentamen H24/aksje oppg1b.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-
 filnavn = "DataFiler/aksjekurs.csv"
 
 datoer = []
 aksjekurs = []
 
 with open(filnavn) as fil:
-        overskrifter = next(fil)
-        print(overskrifter)
+        linje = next(fil)
+        print(linje)
 
         for rad in fil:
             rader = rad.split(";")
-            datoer.append(rader[0])
-            aksjekurs.append(rader[1])
+            date_object = datetime.strptime(rader[0], "%Y-%m-%d")
+            datoer.append( date_object  )
+            aksjekurs.append(float(rader[1]))
 
 plt.plot(datoer, aksjekurs, color="green")
 plt.xlabel("Tid")
 plt.ylabel("Aksjekurs")
 plt.title("Aksjekurs første halvår 2023")
-# plt.xticks(datoer[::10], rotation=90)
-# plt.tight_layout()
+# plt.xticks(datoer[::10], rotation=70)
+plt.tight_layout()
 plt.show()
