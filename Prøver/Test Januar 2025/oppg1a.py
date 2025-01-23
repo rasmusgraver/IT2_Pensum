07afc328e0e38e1d155788b8f5d6330072f90f41
Rasmus Graver <rasmus.graver@gmail.com>
2025-01-23 08:19:32 +0100

    Januar Test

--- Prøver/Test Januar 2025/oppg1a.py
+++ Prøver/Test Januar 2025/oppg1a.py
@@ -1,1 +1,18 @@
-# Kommer... 
+from dyr import Katt, Hund
+
+def dyr_print():
+    hund = Hund("Fido")
+    katt = Katt("Misse")
+
+    hund.hent_ball()
+    hund.hent_ball()
+    katt.spis_mus()
+    katt.spis_mus()
+    katt.spis_mus()
+
+    print(hund)
+    print(katt)
+
+
+
+dyr_print()
--- Prøver/Test Januar 2025/oppg1b.py
+++ Prøver/Test Januar 2025/oppg1b.py
@@ -1,1 +1,20 @@
-# Kommer... 
+from dyr import Katt, Hund
+
+def dyr_til_fil():
+    hund = Hund("Fido")
+    katt = Katt("Misse")
+
+    hund.hent_ball()
+    hund.hent_ball()
+    katt.spis_mus()
+    katt.spis_mus()
+    katt.spis_mus()
+
+    
+    with open("dyr.csv", "w") as fil:
+        fil.write("Type;Navn;Baller;Mus\n")  # Skriver overskriften i filen
+        hund.skriv_til_fil(fil)
+        katt.skriv_til_fil(fil)
+
+
+dyr_til_fil()
