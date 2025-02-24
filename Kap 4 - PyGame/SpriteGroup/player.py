4a43e71540cdaae7c3178da3070b703a7636eb7a
Rasmus Graver <rasmus.graver@gmail.com>
2025-02-24 16:04:18 +0100

    La på en hit box på link

--- Kap 4 - PyGame/SpriteGroup/player.py
+++ Kap 4 - PyGame/SpriteGroup/player.py
@@ -9,7 +9,10 @@ class Player(pg.sprite.Sprite):
         super().__init__()
         self.image = link_r
         self.rect = self.image.get_rect()
+        self.rect.width -= 10
         self.rect.center = (x, y)
+        self.x_adj = 0
+        self.y_adj = 0
 
 
     def update(self):
@@ -17,16 +20,26 @@ class Player(pg.sprite.Sprite):
         if keys_pressed[K_LEFT] and self.rect.x > 0:
             self.rect.x -= PLAYER_SPEED
             self.image = link_l
+            self.x_adj = -10
+            self.y_adj = 0
         if keys_pressed[K_RIGHT] and self.rect.x < SCREEN_WIDTH:
             self.rect.x += PLAYER_SPEED
             self.image = link_r
+            self.x_adj = 0
+            self.y_adj = 0
         if keys_pressed[K_UP] and self.rect.y > 0:
             self.rect.y -= PLAYER_SPEED
             self.image = link_u
+            self.x_adj = -5
+            self.y_adj = 0
         if keys_pressed[K_DOWN] and self.rect.y < SCREEN_HEIGHT:
             self.rect.y += PLAYER_SPEED
             self.image = link_d
+            self.x_adj = -5
+            self.y_adj = 0
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        screen.blit(self.image, (self.rect.x + self.x_adj, self.rect.y + self.y_adj))
+        pg.draw.rect(screen, BLACK, (self.rect.x, self.rect.y, self.rect.width, self.rect.height), 1)
+
         
