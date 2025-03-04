3517e610b1a859c31e51d64c27cb19a8d5968f81
Rasmus Graver <rasmus.graver@gmail.com>
2025-03-04 15:56:14 +0100

    Tegneprogram med mulighet for å holde knapp nede

--- Kap 4B - GUI/Tegneprogram/main.py
+++ Kap 4B - GUI/Tegneprogram/main.py
@@ -36,7 +36,11 @@ brett = [[HVIT for k in range(KOLONNER)] for r in range(RADER)]
 def tegn_ruter():
     for x in range(RADER):
         for y in range(KOLONNER):
-            pass
+            farge = brett[x][y]
+            x_pos = x * CELLESTR
+            y_pos = y * CELLESTR
+            pg.draw.rect(skjerm, farge, (x_pos, y_pos, CELLESTR, CELLESTR))
+            pg.draw.rect(skjerm, GRAA, (x_pos, y_pos, CELLESTR, CELLESTR), 1)
 
 def tegn_palett():
     for i in range(len(FARGER)):
@@ -46,23 +50,51 @@ def tegn_palett():
         pg.draw.rect(skjerm, farge, (x, y, PALETTSTR, PALETTSTR))
 
 
+# Sett opp "initielle verdier"
 running = True
+valgt_farge = FARGER[0]
+tegner = False
+
 while running:
 
     clock.tick(FPS)
     skjerm.fill(HVIT)
 
-    # Sjekk om brukeren avslutter vinduet:
+    # MERK: Vi kan alltid spørre om mouse.get_pos() - også når brukeren ikke klikker
+    # Vi bruker x_pos og y_pos flere steder nedover, så lagrer dem her med en gang
+    x_pos, y_pos = pg.mouse.get_pos()
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
         elif event.type == pg.MOUSEBUTTONDOWN:
-            x_pos, y_pos = pg.mouse.get_pos()
-            print(f"Musklikk ({x_pos}, {y_pos})")
+            tegner = True
+            # print(f"Musklikk ({x_pos}, {y_pos})")
+            if HOYDE - PALETTSTR <= y_pos <= HOYDE:
+                # print("Du er i palett-høyde")
+                if 0 <= x_pos <= len(FARGER)*PALETTSTR:
+                    # print("Du er i palette-bredde")
+                    farge_indeks = x_pos // PALETTSTR
+                    farge = FARGER[farge_indeks]
+                    print(f"Du har valgt farge nr {farge_indeks} som er {farge}")
+                    valgt_farge = farge
+                    tegner = False # Det var ikke museklikk for å begynne å tegne
+
+        elif event.type == pg.MOUSEBUTTONUP:
+            tegner = False
+
+
+    if tegner:
+        x = x_pos // CELLESTR
+        y = y_pos // CELLESTR
+        if x < RADER and y < KOLONNER:
+            brett[x][y] = valgt_farge
 
 
     # Fyll inn oppdatering og tegning av objektene her:
     tegn_ruter()
+    # PASS PÅ: Paletten må tegnes etter rutene - den ligger "oppå" rutene
     tegn_palett()
 
     # Oppdater skjermen for å vise endringene:
