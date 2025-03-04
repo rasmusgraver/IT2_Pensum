05248615c0ee862512633b6fa162149b7afd0ee4
Rasmus Graver <rasmus.graver@gmail.com>
2025-03-04 09:49:41 +0100

    Tegneprogram template

---
+++ Kap 4B - GUI/Tegneprogram/main.py
@@ -0,0 +1,61 @@
+import pygame as pg
+
+# Konstanter
+BREDDE, HØYDE = 800, 600
+RADER, KOLONNER = 20, 20
+CELLESTØRRELSE = BREDDE // KOLONNER
+PALETT_HØYDE = 50
+FPS = 60
+
+# Farger
+HVIT = (255, 255, 255)
+SVART = (0, 0, 0)
+GRÅ = (200, 200, 200)
+FARGER = [
+  (255, 0, 0),   # Rød
+  (0, 255, 0),   # Grønn
+  (0, 0, 255),   # Blå
+  (255, 255, 0), # Gul
+  (255, 165, 0), # Oransje
+  (0, 255, 255), # Cyan
+  (255, 0, 255)  # Magenta
+]
+
+# Initialiser pygame
+pg.init()
+
+# Setter opp skjermen
+skjerm = pg.display.set_mode((BREDDE, HØYDE))
+pg.display.set_caption("Tegnebrett")
+clock = pg.time.Clock()
+
+
+
+brett = [[HVIT for k in range(KOLONNER)] for r in range(RADER)]
+
+
+
+running = True
+while running:
+
+    clock.tick(FPS)
+    skjerm.fill(HVIT)
+
+    # Sjekk om brukeren avslutter vinduet:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            x_pos, y_pos = pg.mouse.get_pos()
+            print(f"Musklikk ({x_pos}, {y_pos})")
+
+
+    # Fyll inn oppdatering og tegning av objektene her:
+
+
+
+    # Oppdater skjermen for å vise endringene:
+    pg.display.update()
+
+# Brukeren har avsluttet programmet, game-loopen er ferdig. Avslutt pygame:
+pg.quit()
