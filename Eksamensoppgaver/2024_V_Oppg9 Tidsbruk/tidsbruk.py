80037df6650ce81c4f4107e0aef2b9626411da25
Rasmus Graver <rasmus.graver@gmail.com>
2025-03-27 10:15:39 +0100

    Tidsbruk "v3" - med funksjoner for filtrering og while running loop

--- Eksamensoppgaver/2024_V_Oppg9 Tidsbruk/tidsbruk.py
+++ Eksamensoppgaver/2024_V_Oppg9 Tidsbruk/tidsbruk.py
@@ -3,20 +3,58 @@ from pprint import pprint
 
 filnavn = "DataFiler/05994_20240126-145813-json.json"
 
+def fiksInnrykk(data):
+    # Rydd opp stygt tegn for inntrykk:
+  for tidsbruk in data:
+      for key in tidsbruk:
+          if key == 'alle aktiviteter':
+              value = tidsbruk[key]
+              if not value[0].isalpha():
+                  # print(f"{key} med value {value}")
+                  tidsbruk[key] = "-> " + value[2:]
+
+def filtrerData(data, filterKey, filterVerdi):
+  nyData = []
+  for tidsbruk in data:
+      for key in tidsbruk:
+          value = tidsbruk[key]
+          if key == filterKey and value == filterVerdi:
+              # Legg til "hele denne bolken" i den nye lista
+              nyData.append(tidsbruk)
+
+  return nyData
+
+def printData(data):
+    print("|" + "-"*66 + "|")
+    print(f'| {"Aktivitet":42}| {"Kjønn":10}|{"Tidsbruk":10}|')
+    print("|" + "-"*66 + "|")
+    for tidsbruk in data:
+            print(f'| {tidsbruk["alle aktiviteter"]:42}| {tidsbruk["kjønn"]:10}|{tidsbruk["Tidsbruk 2000 I alt"]:10}|')
+
+    print("|" + "-"*66 + "|")
+
+
 with open(filnavn, encoding="utf-8") as fil:
     tidsListe = json.load(fil)
 
-
-# Fjern "stygt tegn":
-for ordbok in tidsListe:
-    for key in ordbok.keys():
-        value = ordbok[key]
-        if key == "alle aktiviteter" and not value[0].isalpha():
-            ordbok[key] = "->  " + value[2:]
-
-pprint(tidsListe)
+fiksInnrykk(tidsListe)
+# pprint(tidsListe)
+# print(f"Antall oppføringer: {len(tidsListe)}")
 
 
-for ordbok in tidsListe:
-    print(f'{ordbok["alle aktiviteter"]:42} | {ordbok["kjønn"]:10}')
+running = True
+while running:
+    print("")
+    inp = input("Skriv a,m,k for alle, menn eller kvinner. Skriv q for avslutt: ")
+    if inp == "q":
+        running = False
+    elif inp == "a":
+        filtData = filtrerData(tidsListe, "kjønn", "Alle")
+        printData(filtData)
+    elif inp == "m":
+        filtData = filtrerData(tidsListe, "kjønn","Menn")
+        printData(filtData)
+    elif inp == "k":
+        filtData = filtrerData(tidsListe, "kjønn", "Kvinner")
+        printData(filtData)
 
