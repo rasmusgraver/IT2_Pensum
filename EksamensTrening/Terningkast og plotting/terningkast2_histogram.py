0a376774d1d1b0f75f55e666d3732c16efb659b4
Rasmus Graver <rasmus.graver@gmail.com>
2025-03-31 17:02:05 +0200

    Update terningkast2_histogram.py

--- EksamensTrening/Terningkast og plotting/terningkast2_histogram.py
+++ EksamensTrening/Terningkast og plotting/terningkast2_histogram.py
@@ -2,7 +2,9 @@
 # Det finnes en «rett fram» enkel måte å gjøre dette på. 
 
 # I denne fila:
-# Utfordring: Etter du har gjort den enkle varianten: Kan du lagre alle kastene som en liste med 100 elementer i (hvert kast). Og så undersøke histogram plotting? (HINT: Lag 6 «bins») 
+# Utfordring: Etter du har gjort den enkle varianten: Kan du lagre alle kastene som
+# en liste med 100 elementer i (hvert kast). Og så undersøke histogram plotting?
+# (HINT: Lag 6 «bins») 
 import random
 import matplotlib.pyplot as plt
 
@@ -10,7 +12,7 @@ restultater = []
 
 # MERK! Litt endring fra timen: Morsommere å se på summen av terninger
 # Da får vi flere utfall, og vi kan se at det nærmer seg en normal-fordeling - moro! :) 
-for i in range(1000):
+for i in range(5000):
     kast1 = random.randint(1,6)
     kast2 = random.randint(1,6)
     kast3 = random.randint(1,6)
@@ -18,5 +20,6 @@ for i in range(1000):
 
 # print(restultater)
 
-plt.hist(restultater, bins=7)
+plt.hist(restultater, bins=9, edgecolor='black')
+plt.title('Histogram av terningkast')
 plt.show()
