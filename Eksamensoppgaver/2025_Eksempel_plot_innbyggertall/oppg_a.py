22998e44db95b4b49340a22151c4af496b2f942d
Rasmus Graver <rasmus.graver@gmail.com>
2025-04-01 22:24:20 +0200

    Update folkevekst oppgave

--- Eksamensoppgaver/2025_Eksempel_plot_innbyggertall/oppg_a.py
+++ Eksamensoppgaver/2025_Eksempel_plot_innbyggertall/oppg_a.py
@@ -11,10 +11,11 @@ def printOverskrifter():
     print(f'{"År":5} | {"Fødsler":10} | {"Innflytt":10} | {"Utflytt":10}')
     print("-"*44)
 
-
 def printRad(rad):
     # Bruker > for å høyrestille
     print(f"{rad[0]:>5} | {rad[1]:>10} | {rad[2]:>10} | {rad[3]:>10}")
+
+
 
 with open(filnavn, encoding="utf-8-sig") as fil:
     filinnhold = csv.reader(fil, delimiter=";")
--- Eksamensoppgaver/2025_Eksempel_plot_innbyggertall/oppg_b.py
+++ Eksamensoppgaver/2025_Eksempel_plot_innbyggertall/oppg_b.py
@@ -11,14 +11,16 @@ def printOverskrifter():
     print(f'{"År":5} | {"Fødsler":10} | {"Innflytt":10} | {"Utflytt":10} | {"Folkvekst":10}')
     print("-"*57)
 
-
 def printRad(rad):
     vekst = ".."
     if rad[0] != ".." and rad[1] != ".." and rad[2] != "..":
-        vekst = int(rad[0]) + int(rad[1]) - int(rad[2])
-
+        # Formelen er:    Folkevekst = fødsler+innflytt-utflytt
+        vekst = int(rad[1]) + int(rad[2]) - int(rad[3])
     # Bruker > for å høyrestille
     print(f"{rad[0]:>5} | {rad[1]:>10} | {rad[2]:>10} | {rad[3]:>10} | {vekst:>10}")
+
+
+
 
 with open(filnavn, encoding="utf-8-sig") as fil:
     filinnhold = csv.reader(fil, delimiter=";")
@@ -29,8 +31,7 @@ with open(filnavn, encoding="utf-8-sig") as fil:
     printOverskrifter()
     # Oppgave b) Utvid programmet ved å legge til kolonnen «netto folkevekst» i visningen.
     # Feltene i denne skal være beregnet ut ifra de tre andre.
-    # Formelen er:
-    # Folkevekst = fødsler+innflytt-utflytt
+    # Formelen er:    Folkevekst = fødsler+innflytt-utflytt
     for rad in filinnhold:
         printRad(rad)
 
