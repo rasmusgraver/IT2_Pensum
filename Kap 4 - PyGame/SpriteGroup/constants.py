25b9ad9a6c8bd542bd47ae112d0583df4b91701b
Rasmus Graver <rasmus.graver@gmail.com>
2025-04-03 15:38:02 +0200

    Flyttet og la til flere farger i constants.py

--- Kap 4 - PyGame/SpriteGroup/constants.py
+++ Kap 4 - PyGame/SpriteGroup/constants.py
@@ -1,11 +1,4 @@
 
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-YELLOW = (200, 200 , 0)
-
 # Definerer størrelsen på pygame-vinduet:
 # MERK: Samme størrelse som bg image (om du bruker det)
 SCREEN_WIDTH = 1024
@@ -16,3 +9,18 @@ SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 FPS = 60
 
 PLAYER_SPEED = 5
+
+
+# Noen farger vi kan bruke:
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (200, 200 , 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+ORANGE = (255, 165, 0)
+PURPLE = (128, 0, 128)
+LIGHT_BLUE = (173, 216, 230)
+LIGHT_GREEN = (144, 238, 144)
