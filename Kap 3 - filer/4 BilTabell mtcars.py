fdcca16964e175aba3228ff402f027f58bc39c04
Rasmus Graver <rasmus.graver@gmail.com>
2025-04-24 10:13:32 +0200

    Update 4 BilTabell mtcars.py

--- Kap 3 - filer/4 BilTabell mtcars.py
+++ Kap 3 - filer/4 BilTabell mtcars.py
@@ -1,5 +1,6 @@
 import json
 from pprint import pprint 
+import matplotlib.pyplot as plt
 
 """
 Felter i dataene:
@@ -23,19 +24,39 @@ with open(filnavn, encoding="utf-8") as f:
   data = json.load(f)
 
 biler = []
+ant_cyl = {}
 for bil in data:
     if int(bil["hp"]) > 200:
         print( "HP: " + str(bil["hp"]) + ": " + bil["model"] )
         biler.append(bil)
+    # Legger til i ant_cyl (for alle biler):
+    # Hvis vi har en ny "antall cylindre", så opprett den i ant_cyl:
+    if not bil["cyl"] in ant_cyl:
+        ant_cyl[bil["cyl"]] = 0
+    # Øk antall for den aktuelle "antall cylindre":
+    ant_cyl[bil["cyl"]] += 1
 
 
 # Bruker denne til sorted funksjonen
 def sort_key(b):
 	return b["hp"]
 
-biler = sorted(biler, key=sort_key, reverse=False)
+biler_sortert = sorted(biler, key=sort_key, reverse=False)
 
 print("SORTED")
 print("="*30)
-for bil in biler:
+for bil in biler_sortert:
     print("HP: " + str(bil["hp"]) + ": " + bil["model"] )
+
+def sort_key_cyl(cyl):
+    # Returnerer den første verdien i tuple (som er antall sylindre):
+    return cyl[0]
+
+# Sorter ant_cyl (ut i fra den første verdien - altså antall sylindre):
+ant_cyl_sortert = dict(sorted(ant_cyl.items(), key=sort_key_cyl, reverse=False))
+
+# Lager et sektordiakgram/kakediagram med antall cylindre:
+plt.pie(ant_cyl_sortert.values(), labels=ant_cyl_sortert.keys(), autopct='%1.1f%%', startangle=90, counterclock=False)
+plt.title("Antall sylindre")
+plt.show()
+
